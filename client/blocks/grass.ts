89b23582fa862f9870137288a5c1a4d00fcb6d9c
paulaboks <paulaboks@protonmail.com>
2026-03-27 10:34:11 -0300

    Hoe grass

--- client/blocks/grass.ts
+++ client/blocks/grass.ts
@@ -6,16 +6,14 @@ EverythingRegistry.register<BlockRegistry>("blocks", "bworld:grass", {
 	textures: { top: "bworld:grass_top", bottom: "bworld:dirt", "side": "bworld:grass_side" },
 	has_collision: false,
 	drop_table: "bworld:dirt",
-	/*on_interact(world, tile) {
-		const [player] = world.get_tag("player")!;
+
+	on_interact(dimension, block) {
+		const [player] = dimension.world.get_tag("player")!;
 		const player_inventory = player.get(PlayerComponent)!.player_inventory;
 		const item = player_inventory.container.get_item(player_inventory.hotbar_selected);
 		if (item?.type_id === "bworld:hoe") {
-			tile.id = "bworld:hoed_dirt";
-		} else if (item?.type_id === "bworld:chest") {
-			// world.dimension.add_block(world, { x: tile.x, y: tile.y, id: "bworld:chest" });
-		} else if (item?.type_id === "bworld:furnace") {
-			// world.dimension.add_block(world, { x: tile.x, y: tile.y, id: "bworld:furnace", tickable: true });
+			block.id = "bworld:hoed_dirt";
+			dimension.add_block(block);
 		}
-	},*/
+	},
 });
